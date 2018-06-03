b091f8ce3e66d227c39337cc45c5063e8db41d13
hem sundar <hemsundar@hems-MacBook-Air.local>
2018-06-03 21:45:20 +0530

    best final

--- best.R
+++ best.R
@@ -1,22 +1,25 @@
 #
-best <- function(state=NA, outcome=NA) {
+best <- function(state, outcome) {
       #Read outcome csv file
       outcomefile <- read.csv("outcome-of-care-measures.csv"
                               ,colClasses = "character")
       
       #Subset outcome data frame to get required columns
       bestset <- outcomefile[,c(2,7,11,17,23)]
-      colnames(bestset) <- c("hospital","States","heart attack"
-                             ,"heart failure","pneumonia")
+      colnames(bestset) <- c("hospital","States","ha"
+                             ,"hf","pn")
+      if (outcome == 'heart attack') { col <- 3 }
+      if (outcome == 'heart failure') { col <- 4 }
+      if (outcome == 'pneumonia') { col <- 5 }
       
       if (!any(state == bestset$States)) {
           print("Invalid State")
-      } else if (!any(outcome == colnames(bestset)[3:5])) { 
+      } else if (!any(col %in% c(3,4,5))) { 
             print("Invalid Outcome")
       } else  {
-          ss <- subset(bestset,select = c(hospital,as.character(outcome)),
-                     subset = (States == state))
-          ss <- ss[order(ss$outcome,ss$hospital),]
+          ss <- bestset[bestset$States==state,c(1,col)]
+          ss[,2] <- suppressWarnings(as.numeric(ss[,2]))
+          ss <- ss[order(ss[,2],ss[,1], na.last = NA),]
           paste(ss[1,1])
       }
 }
